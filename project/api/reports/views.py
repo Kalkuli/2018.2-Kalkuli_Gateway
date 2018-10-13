7c6857b96eb0ea8d9b33bbd100da788ad85d368d
Phe0 <pheofo@gmail.com>
2018-10-13 13:52:41 -0300

    Add logic to gateway

--- project/api/reports/views.py
+++ project/api/reports/views.py
@@ -20,10 +20,19 @@ def generate_report():
 def save_report():
 
     date = request.get_json()
-
     receipts = requests.post('http://172.21.0.1:5006/select_date', json= date)
-    report = requests.post('http://172.22.0.1:5004/report', json=receipts.json())
-    response = requests.post('http://172.22.0.1:5004/add_report', json=report.json())
+    report_data = requests.post('http://172.22.0.1:5004/report', json=receipts.json())
+
+    period = date.get('period')
+    date_to = period.get('date_to')
+    date_from = period.get('date_from')
+    total_cost = report_data.json().get('total_cost')
+
+    data = {
+        'date_from': date_from,
+        'date_to': date_to,
+        'total_cost': total_cost
+    }
+    response = requests.post('http://172.22.0.1:5004/add_report', json=data)
 
     return jsonify(response.json()), response.status_code
-
