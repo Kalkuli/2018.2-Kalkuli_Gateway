4d6e6ae1af9b94e5bf53397434a7432d86cfb8f5
Phe0 <pheofo@gmail.com>
2018-10-12 22:26:47 -0300

    Make comunication of dashboard
    Co-authored-by: Pedro Féo <pheofo@gmail.com>
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

--- project/api/reports/views.py
+++ project/api/reports/views.py
@@ -8,6 +8,22 @@ CORS(reports_blueprint)
 
 @reports_blueprint.route('/api/v1/report', methods=['POST'])
 def generate_report():
-    receipts = requests.get('http://kalkuli-receipts.herokuapp.com/receipts')
-    response = requests.post('http://172.24.0.1:5004/report', json=receipts.json())
+
+    date = request.get_json()
+
+    receipts = requests.get('http://172.21.0.1:5006/select_date',json=date.json())
+    response = requests.post('http://172.22.0.1:5004/report', json=receipts.json())
+
     return jsonify(response.json()), response.status_code
+
+@reports_blueprint.route('/api/v1/save_report', methods=['POST'])
+def save_report():
+
+    date = request.get_json()
+
+    receipts = requests.get('http://172.21.0.1:5006/select_date', json= date.json())
+    report = requests.post('http://172.22.0.1:5004/report', json=receipts.json())
+    response = requests.post('http://172.22.0.1:5004/add_report', json=report.json())
+
+    return jsonify(response.json()), response.status_code
+
