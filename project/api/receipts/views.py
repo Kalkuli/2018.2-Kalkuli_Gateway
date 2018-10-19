cfc2861f351a19af26fc878e02e0a1d385c4f7f8
Dutra <ldutra98@gmail.com>
2018-10-19 19:20:59 -0300

    Create route to connect Gateway to receipt's delete method
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/api/receipts/views.py
+++ project/api/receipts/views.py
@@ -28,3 +28,9 @@ def get_receipts():
     response = requests.get('http://kalkuli-receipts.herokuapp.com/receipts')
     return jsonify(response.json()), response.status_code
 
+@receipts_blueprint.route('/api/v1/receipt/<int:receipt_id>', methods=['DELETE'])
+def delete_receipt(receipt_id):
+    response = requests.delete(
+        'http://kalkuli-receipts.herokuapp.com/receipt/%i' % receipt_id
+    )
+    return jsonify(response.json()), response.status_code
