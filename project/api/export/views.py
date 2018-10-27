78a1ac43e3faadc5d215e3ee063a40c6c1be78f5
devsalula <saleh.nazih.dev@gmail.com>
2018-10-27 01:25:38 -0300

    Add structure to Export and implement the  method

---
+++ project/api/export/views.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, Blueprint, request, send_file
+from flask_cors import CORS
+import requests
+
+exports_blueprint = Blueprint('exports', __name__)
+CORS(exports_blueprint)
+
+
+@exports_blueprint.route('/api/v1/export', methods=['POST'])
+def export_csv():
+    
+    date = request.get_json()
+
+    receipts = requests.post('http://kalkuli-receipts.herokuapp.com/select_date',json=date)
+    report = requests.post('http://kalkuli-reports.herokuapp.com/report', json=receipts.json())
+
+    response = requests.post('http://172.26.0.1:5007/export', json=report.json())
+    
+    return response.text, response.status_code
