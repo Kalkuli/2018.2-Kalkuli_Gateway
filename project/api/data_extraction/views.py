b05f7c31be4c93e8c9a02ebccb298c4acb8c4db2
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-11-01 22:57:26 -0300

    Change extraction + interpretation endpoint to interpretation only.
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>

--- project/api/data_extraction/views.py
+++ project/api/data_extraction/views.py
@@ -9,20 +9,14 @@ from project.api.data_extraction.specs.extract import extract_data
 data_extraction_blueprint = Blueprint('data_extraction', __name__)
 CORS(data_extraction_blueprint)
 
-@data_extraction_blueprint.route('/api/v1/extract_data', methods=['POST'])
+@data_extraction_blueprint.route('/api/v1/interpret_data', methods=['POST'])
 @swag_from(extract_data)
-def extract_and_interpret():
-    file = request.files['file']
-    file.name = file.filename
-    files = {'file': file}
-    extract_response = requests.post(
-        'http://172.21.0.1:5001/extract',
-        files=files
-    )
+def interpret():
+    post_data = request.get_json()
 
     intepret_response = requests.post(
         'https://nvv696n4mc.execute-api.sa-east-1.amazonaws.com/dev/interpret',
-        json=extract_response.json()
+        json=post_data
     )
 
     return jsonify({
