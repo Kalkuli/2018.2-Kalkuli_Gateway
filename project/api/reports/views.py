2a9a78e256f396f182be1597b41173575a198544
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-11-15 12:18:39 -0200

    Adjust report related routes to new auth decorators and users service.
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>

--- project/api/reports/views.py
+++ project/api/reports/views.py
@@ -2,47 +2,56 @@ from flask import Flask, jsonify, Blueprint, request
 from flask_cors import CORS
 import requests
 
+from project.api.shared.auth_utils import needs_authentication_with_company_id, needs_authentication
+
 reports_blueprint = Blueprint('reports', __name__)
 CORS(reports_blueprint)
 
 
 @reports_blueprint.route('/api/v1/report', methods=['POST'])
+@needs_authentication
 def generate_report():
-
     date = request.get_json()
+    company_id = date.get('company_id')
 
-    receipts = requests.post('https://kalkuli-receipts-hom.herokuapp.com/select_date',json=date)
-    response = requests.post('https://kalkuli-reports-hom.herokuapp.com/report', json=receipts.json())
+    receipts = requests.post(f'http://172.27.0.1:5006/{company_id}/select_date', json=date)
+    response = requests.post('http://172.28.0.1:5004/report', json=receipts.json())
 
     return jsonify(response.json()), response.status_code
 
 @reports_blueprint.route('/api/v1/save_report', methods=['POST'])
+@needs_authentication
 def save_report():
 
     date = request.get_json()
-    receipts = requests.post('https://kalkuli-receipts-hom.herokuapp.com/select_date', json= date)
-    report_data = requests.post('https://kalkuli-reports-hom.herokuapp.com/report', json=receipts.json())
+    company_id = date.get('company_id')
+
+    receipts = requests.post(f'http://172.27.0.1:5006/{company_id}/select_date', json=date)
+    report_data = requests.post('http://172.28.0.1:5004/report', json=receipts.json())
 
     period = date.get('period')
     date_to = period.get('date_to')
     date_from = period.get('date_from')
 
     data = {
+        'company_id': company_id,
         'date_from': date_from,
         'date_to': date_to
     }
-    response = requests.post('https://kalkuli-reports-hom.herokuapp.com/add_report', json=data)
+    response = requests.post('http://172.28.0.1:5004/add_report', json=data)
 
     return jsonify(response.json()), response.status_code
 
-@reports_blueprint.route('/api/v1/get_all_reports', methods=['GET'])
-def get_all_reports():
-    response = requests.get('https://kalkuli-reports-hom.herokuapp.com/get_reports')
+@reports_blueprint.route('/api/v1/<company_id>/get_all_reports', methods=['GET'])
+@needs_authentication_with_company_id
+def get_all_reports(company_id):
+    response = requests.get(f'http://172.28.0.1:5004/{company_id}/get_reports')
     return jsonify(response.json()), response.status_code
 
-@reports_blueprint.route('/api/v1/report/<int:report_id>', methods=['DELETE'])
-def delete_report(report_id):
+@reports_blueprint.route('/api/v1/<company_id>/report/<int:report_id>', methods=['DELETE'])
+@needs_authentication_with_company_id
+def delete_report(company_id, report_id):
     response = requests.delete(
-        'https://kalkuli-reports-hom.herokuapp.com/report/%i' % report_id
+        f'http://172.28.0.1:5004/{company_id}/report/{report_id}'
     )
     return jsonify(response.json()), response.status_code
